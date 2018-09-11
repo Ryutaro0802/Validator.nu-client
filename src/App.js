9b07dde3a347100c954f764138300f916c8e43c4
Ryutaro Kawakami <ne200152g@gmail.com>
2018-09-11 10:23:20 +0900

    URLを取得してエラーを表示

--- src/App.js
+++ src/App.js
@@ -9,29 +9,35 @@ export class App {
   }
 
   fetch() {
-    const params = new URLSearchParams();
-    params.set("doc", "https://www.google.com/");
-    params.set("out", "json");
-    params.set("level", "error");
-    function fetchData(url, data) {
+    const fetchData = (url, data) => {
       return fetch(url).then(response => response.json());
     }
-    fetchData(`https://html5.validator.nu/?${params.toString()}`).then(
-      data => this.mount(data.messages)
-    );
+    chrome.tabs.getSelected(null, tab => {
+      const params = new URLSearchParams();
+      params.set("doc", tab.url);
+      params.set("out", "json");
+      params.set("level", "error");    
+      fetchData(`https://html5.validator.nu/?${params.toString()}`)
+        .then(data => this.mount(data.messages))
+        .catch(error => {
+          alert(error);
+        });
+    });
   }
 
   mount(messages) {
     const appElement = document.getElementById("app");
     let errors = [];
     messages.forEach(message => {
-      errors.push(new ErrorItemModel({
+      errors.push(
+        new ErrorItemModel({
           extract: message.extract,
           line: message.lastLine,
           message: message.message
-        }));
+        })
+      );
     });
-    this.errorItemListModel = new ErrorItemListModel(errors = errors);
+    this.errorItemListModel = new ErrorItemListModel((errors = errors));
     const errorItems = this.errorItemListModel.getErrors();
     const errorListElement = this.errorListView.createElement(errorItems);
     render(errorListElement, appElement);
